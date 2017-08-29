76736065863dd612bcbe3f3521690f2e2998adff
Sasakorn Kulchittipakk <osasakorn@gmail.com>
2017-08-29 19:50:53 +0700

    bpy

---
+++ build/main.bundle.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function love(gain) {
+	document.getElementById("key").innerHTML = document.getElementById('key').innerHTML + gain;
+}
+
+function lov() {
+	var g = document.getElementById('key').innerHTML;
+	var x = document.getElementById("key").innerHTML.length;
+	document.getElementById('key').innerHTML = g.substring(0, x - 1);
+}
+function bar(gain) {
+	document.getElementById("key").innerHTML = document.getElementById('key').innerHTML + ' ';
+}
+function cle() {
+	document.getElementById("key").innerHTML = ' ';
+}
